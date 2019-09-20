8b912b8a136297d3f57d4ed5d4d32c5ee236d820
Sogbesan Habib <h.d.sogbesan@gmail.com>
2019-09-20 04:49:30 +0100

    Working app.

---
+++ public/src/js/app.js
@@ -0,0 +1,39 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    query: '',
+    page: 1,
+    results: [],
+    processing: false
+  },
+  methods: {
+    async fetchResults() {
+      try {
+        if (this.page === 1) {
+          this.results = [];
+        }
+        this.processing = true;
+        const resp = await axios.get('https://api.unsplash.com/search/photos', {
+          params: {
+            page: this.page,
+            query: this.query
+          },
+          headers: {
+            Authorization: `Client-ID 5da5bf6418e04d2ceee5e4ff1f64204e3fdda31a0bbf75c848c4fca0c29c95f7`
+          }
+        });
+        const {results} = resp.data;
+        console.log(results, 'results');
+        if (this.page === 1) {
+          this.results = results;
+        } else {
+          this.results = this.results.concat(results);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.processing = false;
+      }
+    }
+  }
+});
